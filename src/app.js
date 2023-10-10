d5d1c288fed92bc445807c99261532a868005119
heybadrinath <ram.r@kalvium.community>
2023-10-10 16:07:47 +0530

    src/app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ let currentTeam = "Manchester FC";
 let trophiesWon = 27;
 
 //Write your function here
+function createManager(managerName, managerAge, currentTeam, trophiesWon) {
+  return [managerName, managerAge, currentTeam, trophiesWon];
+}
+
 
 // Don't edit the following code
 try {
@@ -23,6 +27,19 @@ var formation = [4, 4, 3];
 
 //write your function here
 
+function createFormation(num) {
+  if(num.length==0){
+    return null
+  }else{
+    var format =  {
+      defender: num[0],
+      midfield: num[1],
+      forward: num[2]
+  }
+}
+  return format
+}
+
 // Dont edit the following code
 
 try {
@@ -33,11 +50,34 @@ try {
 
 //Progression 3 - Filter players that debuted in ___ year
 
+function filterByDebut(year) {
+  return players.filter((players) => players.debut == year)
+}
+
 //Progression 4 - Filter players that play at the position _______
+
+function filterByPosition(position) {
+  return players.filter((players)=> players.position == position )
+}
 
 //Progression 5 - Filter players that have won ______ award
 
+function filterByAward(awardName) {
+  return players.filter((player) => {
+    return player.awards.some((item) => {
+      return item.name===awardName})
+  })
+}
+
 //Progression 6 - Filter players that won ______ award ____ times
+
+function filterByAwardxTimes(awardName, noOfTimes) {
+  return players.filter((player) => {
+    let count = player.awards.filter((item) => {
+      return item.name === awardName;
+    })
+  })
+}
 
 //Progression 7 - Filter players that won ______ award and belong to ______ country
 
@@ -45,7 +85,7 @@ try {
 
 //Progression 9 - Sort players in descending order of their age
 
-//Progression 10 - Sort players beloging to _____ team in descending order of awards won
+//Progreturnteam in descending order of awards won
 
 //Challenge 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
 
